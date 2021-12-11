1d825817967ebfb3febf4e29ecc411191e2106c5
Nursyafriady Yadi <nursyafriady83@gmail.com>
2021-12-11 21:10:19 +0800

    Stateless-hook

--- src/App.js
+++ src/App.js
@@ -1,69 +1,63 @@
-import React, { Component } from 'react';
-import axios from 'axios'
+import React, { useState } from 'react';
 
-class App extends Component {
-      constructor () {
-          super() 
+function App() {
+    const [firstname, setFirstName] = useState('')
+    const [lastname, setLastName] = useState('')
+    const [name, setName] = useState('')
 
-          this.state = {
-              firstname: '',
-              lastname: '',
-              name: ''
-          }
-      }
+    // menggunakan hook state
+    const submitHandler = (e) => {
+        e.preventDefault()
+        setName(`${firstname} ${lastname}`)
+        setFirstName('')
+        setLastName('')
+    }
 
-      changehandler = (e) => {
-          this.setState({
-              [e.target.name]: e.target.value // Form name
-          })
-      }
-
-      submitHandler = (e) => {
-          e.preventDefault()
-          this.setState({
-            name: `${this.state.firstname} ${this.state.lastname}` 
-          })
-
-          this.state.firstname = ''
-          this.state.lastname = ''
-
-          // dibaca sebelum dirender
-      }
-
-      render() {
-        const {name} = this.state 
-          return (
-              <div className="p-5">
-                  <div className="container">
+    return (
+        <div className="p-5">
+              <div className="container">
+                  <div className="row">
                       <div className="col-md-4">
                           <div className="card">
-                              <div className="card-header">
-                                  Learn React JS
-                              </div>
-                              <div className="card-body">
-                                  <form onSubmit={this.submitHandler}>
-                                      <div className="mb-4">
-                                          <label htmlFor="firstname" className="form-label">First Name</label>
-                                          <input type="text" value={this.state.firstname} onChange={this.changehandler} name="firstname" id="firstname" className="form-control"></input>
-                                      </div>
-                                      <div className="mb-4">
-                                          <label htmlFor="lastname" className="form-label">Last Name</label>
-                                          <input type="text" value={this.state.lastname} onChange={this.changehandler} name="lastname" id="lastname" className="form-control"></input>
-                                      </div>
-                                      <div className="d-grid gap-2">
-                                          <button type="submit" className="btn btn-primary">Show</button>
-                                      </div>
-                                  </form>
-                              </div>
-                              <div className="card-footer">
-                                  My Name is {name ? name : '. . .'}
-                              </div>
+                                <div className="card-header">Learn React Function</div>
+                                <div className="card-body">
+                                    <form onSubmit={submitHandler}>
+                                        <div className="mb-4">
+                                              <label htmlFor="firstname" className="form-label">First Name</label>
+                                              <input 
+                                                type="text" 
+                                                name="firstname" 
+                                                id="firstname" 
+                                                className="form-control"
+                                                value={firstname}
+                                                onChange={(e) => setFirstName(e.target.value)}
+                                              ></input>
+                                        </div>
+                                        <div className="mb-4">
+                                              <label htmlFor="lastname" className="form-label">Last Name</label>
+                                              <input 
+                                                type="text" 
+                                                name="lastname" 
+                                                id="lastname" 
+                                                className="form-control"
+                                                value={lastname}
+                                                onChange={(e) => setLastName(e.target.value)}
+                                              ></input>
+                                        </div>
+                                        <div className="d-grid gap-2">
+                                            <button type="submit" className="btn btn-primary">Show</button>
+                                        </div>
+                                    </form>
+                                </div>
+                                <div className="card-footer">
+                                    My Name is {name ? name : '. . .'}
+                                </div>
                           </div>
                       </div>
                   </div>
               </div>
-            )
-      }
+        </div>
+    )
 }
 
 export default App;
---
+++ src/components/ClassExample.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+
+class ClassExample extends Component {
+      constructor () {
+          super() 
+
+          this.state = {
+              firstname: '',
+              lastname: '',
+              name: ''
+          }
+      }
+
+      changehandler = (e) => {
+          this.setState({
+              [e.target.name]: e.target.value // Form name
+          })
+      }
+
+      submitHandler = (e) => {
+          e.preventDefault()
+          this.setState({
+            name: `${this.state.firstname} ${this.state.lastname}` 
+          })
+
+          this.state.firstname = ''
+          this.state.lastname = ''
+
+          // dibaca sebelum dirender
+      }
+
+      render() {
+        const {name} = this.state 
+          return (
+              <div className="p-5">
+                  <div className="container">
+                      <div className="col-md-4">
+                          <div className="card">
+                              <div className="card-header">
+                                  Learn React JS
+                              </div>
+                              <div className="card-body">
+                                  <form onSubmit={this.submitHandler}>
+                                      <div className="mb-4">
+                                          <label htmlFor="firstname" className="form-label">First Name</label>
+                                          <input type="text" value={this.state.firstname} onChange={this.changehandler} name="firstname" id="firstname" className="form-control"></input>
+                                      </div>
+                                      <div className="mb-4">
+                                          <label htmlFor="lastname" className="form-label">Last Name</label>
+                                          <input type="text" value={this.state.lastname} onChange={this.changehandler} name="lastname" id="lastname" className="form-control"></input>
+                                      </div>
+                                      <div className="d-grid gap-2">
+                                          <button type="submit" className="btn btn-primary">Show</button>
+                                      </div>
+                                  </form>
+                              </div>
+                              <div className="card-footer">
+                                  My Name is {name ? name : '. . .'}
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+            )
+      }
+}
+
+export default ClassExample;
