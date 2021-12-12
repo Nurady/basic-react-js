67e6e46491bef8e39cb61b3651ce8c78e18d43a4
Nursyafriady Yadi <nursyafriady83@gmail.com>
2021-12-12 10:39:57 +0800

    React Router Dom

--- src/App.js
+++ src/App.js
@@ -1,84 +1,16 @@
-import axios from 'axios';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import Navbar from './components/Navbar';
+import Router from './router';
 
-function App() {
-    const [identifier, setIdentifier] = useState(1)
-    const [users, setUsers] = useState([])
-    const [loading, setLoading] = useState(false)
-
-    const getUsers = async() => {
-        setLoading(true)
-        try {
-            let response = await axios.get(`https://jsonplaceholder.typicode.com/users/${identifier}`)
-            setUsers(response.data)
-            setLoading(false)
-        } catch (error) {
-            console.log(error.message)
-            setLoading(false)
-        }
-    }
-
-    useEffect(() => {
-        getUsers()       
-    },[identifier])
-  
+function App(props) {
     return (
-      <div className="py-5">
-          <div className="container">
-              <div className="row justify-content-center">
-                  <div className="col-md-8">
-                        <input 
-                            type="text" 
-                            name="identifier" 
-                            id="identifier" 
-                            className="form-control" 
-                            value={identifier}
-                            onChange={(e) => setIdentifier(e.target.value)}
-                        />
-                        {
-                            loading 
-                            ? 
-                            <div>Loading . . . .</div> 
-                            :
-                            <table className="table">
-                                <thead>
-                                    <tr>
-                                        <th>Name</th>
-                                        <th>Username</th>
-                                        <th>Email</th>
-                                        <th>Website</th>
-                                        <th>Phone</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>{users.name}</td>
-                                        <td>{users.Username}</td>
-                                        <td>{users.email}</td>
-                                        <td>{users.website}</td>
-                                        <td>{users.phone}</td>
-                                    </tr>
-                                    {/* {
-                                        users.map((user, index) => {
-                                            return (
-                                                <tr key={index}>
-                                                    <td>{user.name}</td>
-                                                    <td>{user.Username}</td>
-                                                    <td>{user.email}</td>
-                                                    <td>{user.website}</td>
-                                                    <td>{user.phone}</td>
-                                                </tr>
-                                            )
-                                        })
-                                    } */}
-                                </tbody>
-                            </table>
-                        }
-                  </div>
-              </div>
-          </div>
-      </div> 
-    )
+        <div>
+            <Navbar />
+            <div className="py-4">
+                <Router />
+            </div>
+        </div>
+    );
 }
 
 export default App;
