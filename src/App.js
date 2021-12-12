8e3ec9c90b28ba3a2820ee45e1695b242a79ca1a
Nursyafriady Yadi <nursyafriady83@gmail.com>
2021-12-12 08:45:13 +0800

    Fetching Data Dengan UseEffect

--- src/App.js
+++ src/App.js
@@ -1,40 +1,83 @@
-import React from 'react';
-import Card from './components/Card';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+    const [identifier, setIdentifier] = useState(1)
+    const [users, setUsers] = useState([])
+    const [loading, setLoading] = useState(false)
+
+    const getUsers = async() => {
+        setLoading(true)
+        try {
+            let response = await axios.get(`https://jsonplaceholder.typicode.com/users/${identifier}`)
+            setUsers(response.data)
+            setLoading(false)
+        } catch (error) {
+            console.log(error.message)
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        getUsers()       
+    },[identifier])
+  
     return (
-        <div className="py-4">
-            <div className="container">
-                <h4>The Post</h4>
-                <hr />
-                <div className="row">
-                    <div className="col-md-4">
-                        <Card content={{
-                                imageUrl:"http://placekitten.com/g/200/200", 
-                                title:"The first Post", 
-                                published:"Published 06 Februari 2021"
-                            }}
+      <div className="py-5">
+          <div className="container">
+              <div className="row justify-content-center">
+                  <div className="col-md-8">
+                        <input 
+                            type="text" 
+                            name="identifier" 
+                            id="identifier" 
+                            className="form-control" 
+                            value={identifier}
+                            onChange={(e) => setIdentifier(e.target.value)}
                         />
-                    </div>
-                    <div className="col-md-4">
-                        <Card content={{
-                                imageUrl:"http://placekitten.com/g/200/200", 
-                                title:"The second Post", 
-                                published:"Published 06 Februari 2020"
-                            }}
-                        />
-                    </div>
-                    <div className="col-md-4">
-                        <Card content={{
-                                imageUrl:"http://placekitten.com/g/200/200", 
-                                title:"The third Post", 
-                                published:"Published 06 Februari 2019"
-                            }}
-                        />
-                    </div>
-                </div>
-            </div>
-        </div>
+                        {
+                            loading 
+                            ? 
+                            <div>Loading . . . .</div> 
+                            :
+                            <table className="table">
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Username</th>
+                                        <th>Email</th>
+                                        <th>Website</th>
+                                        <th>Phone</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>{users.name}</td>
+                                        <td>{users.Username}</td>
+                                        <td>{users.email}</td>
+                                        <td>{users.website}</td>
+                                        <td>{users.phone}</td>
+                                    </tr>
+                                    {/* {
+                                        users.map((user, index) => {
+                                            return (
+                                                <tr key={index}>
+                                                    <td>{user.name}</td>
+                                                    <td>{user.Username}</td>
+                                                    <td>{user.email}</td>
+                                                    <td>{user.website}</td>
+                                                    <td>{user.phone}</td>
+                                                </tr>
+                                            )
+                                        })
+                                    } */}
+                                </tbody>
+                            </table>
+                        }
+                  </div>
+              </div>
+          </div>
+      </div> 
     )
 }
 
